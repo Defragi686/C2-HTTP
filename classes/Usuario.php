30fb5347680731b3c25bc8af3f560335e7c6621d
Kanui <65466003+Kanui-C@users.noreply.github.com>
2021-04-06 00:28:03 -0300

    Update Usuario.php

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -1,7 +1,6 @@
 <?php
 
-    Class Usuario
-    {
+    Class Usuario{
         private $sql;
         public $msgError = "";
         public function conectar($banco, $nome, $host, $usuario, $senha){
@@ -13,27 +12,27 @@
                 $msgError = $e->getMessage();
             }
             
-        }
+     }
 
-        public function logar($email, $senha){
-            global $sql;
-            
-            $pdo = $sql->prepare("SELECT id FROM usuarios WHERE senha=:s AND email=:e");
-            $pdo->BindValue(":e",$email);
-            $pdo->BindValue(":s",$senha);
-            $pdo->execute();
-            if($pdo->rowCount() > 0){
-                $dado = $pdo->fetch();
-                session_start();
-                $_SESSION['id'] = $dado['id'];
-                $_SESSION['email'] = $dado['email'];
-                return true;
-            }else
-            {
-                echo $pdo->rowCount();
-                return false; //Nao foi possivel logar
-            }
+    public function logar($email, $senha){
+        global $sql;
+
+        $pdo = $sql->prepare("SELECT id FROM usuarios WHERE senha=:s AND email=:e");
+        $pdo->BindValue(":e",$email);
+        $pdo->BindValue(":s",$senha);
+        $pdo->execute();
+        if($pdo->rowCount() > 0){
+            $dado = $pdo->fetch();
+            session_start();
+            $_SESSION['id'] = $dado['id'];
+            $_SESSION['email'] = $dado['email'];
+            return true;
+        }else
+        {
+            echo $pdo->rowCount();
+            return false; //Nao foi possivel logar
         }
+    }
         /*Costumize sua busca!*/
 
     }
